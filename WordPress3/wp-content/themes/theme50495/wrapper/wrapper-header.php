2f60ab61663c583710c1cc4830a229927144ee2d
Antoine Taillefer <ataillefer@nuxeo.com>
2014-09-09 23:56:48 +0200

    Wordpress using 50495 theme with a lot of overridden php / css...

---
+++ WordPress3/wp-content/themes/theme50495/wrapper/wrapper-header.php
@@ -0,0 +1,14 @@
+<?php /* Wrapper Name: Header */ ?>
+<div class="tail-top">
+	<div class="row">
+		<div class="span4" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
+			<?php get_template_part("static/static-logo"); ?>
+		</div>
+		<div class="span8" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+			<?php get_template_part("static/static-nav"); ?>
+		</div>
+	</div>
+	<div class="header-bottom">
+		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	</div>
+</div>
